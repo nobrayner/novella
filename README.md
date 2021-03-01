d4c1c4d1095eba2db3793ec9d68f1acc791dc001
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-03-01 11:15:15 +1100

    Working POC 🥳

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# novella README
+<p align="center">
+  <h1>Novella</h1>
+</p>
+<p align="center">Build UI components from the comfort of Visual Studio Code</p>
 
-This is the README for your extension "novella". After writing up a brief description, we recommend including the following sections.
+Novella is a Visual Studio Code extension inspired by Storybook. View the UI component that you are working on side-by-side with the code, and watch it update live.
+
+_GIF TBD_
+
+---
 
 ## Features
 
@@ -14,57 +21,10 @@ For example if there is an image subfolder under your extension project workspac
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+- _Something about webpack knowledge, setting up a config relevant for your environment, etc._
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- `novella.webpackConfigFile`: The file and path - relative the root of the workspace - to read the novella webpack config file from
---
+++ src/PreviewPanel.ts
@@ -0,0 +1,227 @@
+import * as vscode from 'vscode'
+import path from 'path'
+import {
+  Compiler as WebpackCompiler,
+  Stats as WebpackCompileStats,
+} from 'webpack'
+// import { getNonce } from "./getNonce";
+
+export class PreviewPanel {
+  // Track the currentl panel. Only allow a single panel to exist at a time.
+  public static currentPanel: PreviewPanel | undefined
+
+  public static readonly viewType = 'novella-preview'
+
+  private readonly _panel: vscode.WebviewPanel
+  private _compiler: WebpackCompiler
+  private _document: vscode.TextDocument
+  private _lastCompilationHash: string | undefined
+
+  private readonly _extensionUri: vscode.Uri
+  private _disposables: vscode.Disposable[] = []
+
+  public static createOrShow(
+    extensionUri: vscode.Uri,
+    document: vscode.TextDocument,
+    compiler: WebpackCompiler
+  ) {
+    const viewColumn = vscode.ViewColumn.Beside
+
+    // If we already have a panel, show it.
+    if (PreviewPanel.currentPanel) {
+      PreviewPanel.currentPanel.trackDocument(document, compiler)
+      PreviewPanel.currentPanel._panel.reveal(viewColumn)
+      return
+    }
+
+    // Otherwise, create a new panel.
+    const panel = vscode.window.createWebviewPanel(
+      PreviewPanel.viewType,
+      `Preview ${path.basename(document.fileName)}`,
+      viewColumn,
+      {
+        // Enable javascript in the webview
+        enableScripts: true,
+
+        // And restrict the webview to only loading content from our extension's `media` directory.
+        localResourceRoots: [extensionUri],
+      }
+    )
+
+    PreviewPanel.currentPanel = new PreviewPanel(
+      panel,
+      extensionUri,
+      document,
+      compiler
+    )
+    PreviewPanel.currentPanel.trackDocument(document, compiler)
+  }
+
+  public static kill() {
+    PreviewPanel.currentPanel?.dispose()
+    PreviewPanel.currentPanel = undefined
+  }
+
+  private constructor(
+    panel: vscode.WebviewPanel,
+    extensionUri: vscode.Uri,
+    document: vscode.TextDocument,
+    compiler: WebpackCompiler
+  ) {
+    this._panel = panel
+    this._extensionUri = extensionUri
+    this._document = document
+    this._compiler = compiler
+
+    // Set the webview's initial html content
+    this._update()
+
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables)
+
+    // Handle messages from the webview
+    this._panel.webview.onDidReceiveMessage(
+      this._onDidReceiveMessage,
+      null,
+      this._disposables
+    )
+  }
+
+  public dispose() {
+    PreviewPanel.currentPanel = undefined
+
+    // Clean up our resources
+    this._panel.dispose()
+    this._compiler.close(() => {
+      console.log(`Stopped watching ${this._document.fileName}`)
+    })
+
+    while (this._disposables.length) {
+      const x = this._disposables.pop()
+      if (x) {
+        x.dispose()
+      }
+    }
+  }
+
+  public trackDocument(
+    document: vscode.TextDocument,
+    compiler: WebpackCompiler
+  ) {
+    this._compiler.close(() =>
+      console.log(`Stopped watching ${this._document.fileName}`)
+    )
+
+    this._document = document
+    this._compiler = compiler
+
+    this._panel.title = `Preview ${path.basename(document.fileName)}`
+    this._update()
+
+    // Start the compiler to watch for changes
+    this._compiler.watch({}, this._onWebpackCompile) //this._onWebpackCompile)
+    console.log(`Watching ${this._document.fileName} for changes...`)
+  }
+
+  private _update() {
+    this._panel.webview.html = this._getHtmlForWebview(this._panel.webview)
+  }
+
+  private _onDidReceiveMessage(message: any) {
+    switch (message.command) {
+      case 'info':
+        vscode.window.showInformationMessage(message.text)
+        break
+      case 'error':
+        vscode.window.showErrorMessage(message.text)
+    }
+  }
+
+  private _onWebpackCompile(error?: Error, result?: WebpackCompileStats) {
+    if (error) {
+      console.error(error)
+      return
+    }
+
+    const stats = result!
+    if (this._lastCompilationHash === stats.hash) {
+      return
+    }
+
+    this._lastCompilationHash = stats.hash
+    const info = stats.toJson()
+
+    if (stats.hasErrors()) {
+      console.error(`Compilation completed with ${info.errorsCount} errors:`)
+      info.errors?.forEach((error) => {
+        console.error(error.message)
+      })
+    } else if (stats.hasWarnings()) {
+      console.warn(`Compilation completed with ${info.warningsCount} warnings:`)
+      info.warnings?.forEach((warning) => {
+        console.warn(warning.message)
+      })
+    } else {
+      console.log(`Compilation completed successfully!`)
+      const componentCode = PreviewPanel.currentPanel?._compiler.outputFileSystem
+        // @ts-ignore
+        .readFileSync('/dist/component.js')
+        .toString()
+      if (PreviewPanel.currentPanel) {
+        PreviewPanel.currentPanel._panel.webview.html = PreviewPanel.currentPanel._getHtmlForWebview(
+          PreviewPanel.currentPanel._panel.webview,
+          componentCode
+        )
+      }
+    }
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview, componentCode?: string) {
+    const reactUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionUri,
+        'node_modules/react/umd',
+        'react.development.js'
+      )
+    )
+    const reactDomUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionUri,
+        'node_modules/react-dom/umd',
+        'react-dom.development.js'
+      )
+    )
+
+    return `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <div id="root"></div>
+  <script>
+    (function () {
+      const vscode = acquireVsCodeApi()
+      vscode.postMessage({
+        command: 'info',
+        text: 'A message from the extension!'
+      })
+    }())
+  </script>
+  <script src="${reactUri}"></script>
+  <script src="${reactDomUri}"></script>
+  ${
+    componentCode
+      ? `<script>
+      ${componentCode}
+      ReactDOM.render(Component.default(), document.getElementById('root'))
+    </script>`
+      : ''
+  }
+</body>
+</html>`
+  }
+}
